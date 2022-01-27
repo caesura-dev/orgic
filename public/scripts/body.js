fb13058147de2152fd65a1d71ada6d9510cf7fe6
a_Sharp_a <izzymacster@gmail.com>
2022-01-26 16:31:06 -0800

    added support for odt files

--- public/scripts/body.js
+++ public/scripts/body.js
@@ -1,14 +1,30 @@
-function body(file)
+function body(file, type)
 {
     var doc;
     fetch(file)
         .then(response => response.arrayBuffer())
         .then(ab => {
-            mammoth.convertToHtml({arrayBuffer : ab})
-            .then(function(result){
-                var html = result.value; // The generated HTML
-                document.body.innerHTML = html;
-            })
-            .done();
+            if (type == "docx")
+            {
+                mammoth.convertToHtml({arrayBuffer : ab})
+                .then(function(result){
+                    var html = result.value; // The generated HTML
+                    document.body.innerHTML = html;
+                })
+                .done();
+            }
+            if (type == "odt")
+            {
+                try 
+                {
+                    html = new ODTDocument(ab).getHTML();
+                    document.body.innerHTML = html;
+                } 
+                catch(e) 
+                {
+                    alert("Couldn't parse odt file.");
+                    throw e;
+                }
+            }
         })
 }
